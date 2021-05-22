4b9538950d587f2b21efb3b62d0353bc6bae66cc
bomi <bonita9931@gmail.com>
2021-05-22 20:19:56 +0900

    db mysql로 변경

--- gp/settings.py
+++ gp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'gp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gp-database',
+        'USER': 'gp',
+        'PASSWORD': '1234qwer',
+        'HOST': 'gp-database.cfgbs9nzcfco.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
